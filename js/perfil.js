801f952e67fd2fd799aa283710d3c5900b1c3ee9
Keily <kbtelles@scl.edu.gt>
2023-06-08 08:42:26 -0600

    Perfil de usuario terminado

--- js/perfil.js
+++ js/perfil.js
@@ -1,19 +1,24 @@
 let codigo_perfil = `
     <div class="portada"></div>
-    <div class="fotoperfil">
-    <img class="foto" src="https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Pic.png" alt="">
-    </div>
-    <section class="caja1">
-        <div class="info">
+    <section class="todo">
+
+    <section class="caja">
+        <div class="fotoperfil">
+        <img class="foto" src="https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Pic.png" alt="">
+
         <h2 class="name">Leanne Graham</h2>
         <h1 class="user">@Bret</h1>
         </div>
+    </section>
+
+    <section class="caja1">
         <div class="iconos">
         <img class="mundo" src="https://cdn-icons-png.flaticon.com/512/1089/1089292.png" alt="">
-        <img class="check" src="https://cdn-icons-png.flaticon.com/512/44/44796.png" alt="">
-        <img class="seguir" src="https://png.pngtree.com/png-clipart/20230124/ourmid/pngtree-follow-button-png-image_6571180.png" alt="">
-        </div>
+        <img class="check" src="https://cdn.icon-icons.com/icons2/2368/PNG/512/check_box_double_icon_143825.png" alt="">
+        <h1 class="seguir">Seguir  </h1>
     </section>
+    </section>
+
     <div class="secciones">
         <div class="posit"> 
         <h1 class="text">Post</h1>
